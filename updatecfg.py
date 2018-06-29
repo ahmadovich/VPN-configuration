3fc307c37415d88444c087392fe282ab226a6887
Ahmad Hamdy <ahmad.hamdy@gmail.com>
2018-06-29 13:44:34 +0200

    Adding comments

--- updatecfg.py
+++ updatecfg.py
@@ -1,6 +1,6 @@
 import argparse
 import os
-from test.test_decimal import file
+
 
 # Collecting command line arguments
 parser = argparse.ArgumentParser('updatecfg')
@@ -10,21 +10,26 @@ parser.add_argument('-a', '--authfile', required = False, type = str, nargs = '+
 args = parser.parse_args()
 
 def updatefile():
+    # Parse dirrectories and do some validation
     for dirz in args.dir:
         if not os.path.isdir(dirz):
             print('\n' + dirz + ' is not a directory, please check\n')
             exit(1)
+        # Identify openvpn configuration files and open them read-only  
         for (curdir, subdirs, filenames) in os.walk(dirz) :
             for file in filenames:
                 if os.path.splitext(file)[1] == ".ovpn":
                     filepath = os.path.join(curdir,file)
                     with open (filepath,'r') as infile:
+                        # Creating temp files with .txt extensions
                         tempname = (filepath + '.txt')
+                        # Open tmp files for writing and modify the auth-user-pass line
                         with open(tempname,'w') as outfile :
                             for line in infile.readlines():
                                 if 'auth-user-pass'.lower() in line.lower():
                                     line = 'auth-user-pass readme1.txt\n'
                                 outfile.write(line)
+                    # Do final cleanup, remove original file and rename tmp file to be same as original
                     os.remove(filepath)
                     os.rename(tempname,filepath)
                     
